75624cb4eb274bd9ba634d2cdc7e7544af6fda55
Mauricio Nunes <mutila@gmail.com>
2021-11-22 22:53:18 -0300

    All Headless Wallet func inserted in UnityHathor

--- Scripts/HathorConnect.cs
+++ Scripts/HathorConnect.cs
@@ -3,18 +3,19 @@ using System;
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.JSONSerializeModule;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.IO;
 
 namespace UnityHathor
-{    
-    
+{
+
 
     public class HathorConnect : MonoBehaviour
     {
-        public static string login;        
+        public static string login;
 
         // seedKey configurada na hathor wallet headless, o correto � ter uma exclusiva para a conta que ter� todas os tokens e uma seedKey para os jogadores
         private static string seedKey = "default";
@@ -26,12 +27,15 @@ namespace UnityHathor
 
         //public Text logInputTextValue;
 
-        // Retorna em JSON o balanco da carteira do jogador.
-        public static IEnumerator WalletBalance()
+        // Return the balance of HTR
+        public static IEnumerator WalletBalance(string token=null)
         {
             UnityWebRequest www = null;
 
-            www = UnityWebRequest.Get("http://localhost:8000/wallet/balance");
+            if(token!=null)
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/balance?token="+token);
+            else
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/balance");
 
 
             www.SetRequestHeader("X-Wallet-Id", login);
@@ -55,7 +59,35 @@ namespace UnityHathor
             }
         }
 
-        // Retorna o status da carteira.
+        // Get the index of an address
+        public static IEnumerator AddressIndex(string address)
+        {
+            UnityWebRequest www = null;
+
+            www = UnityWebRequest.Get("http://localhost:8000/wallet/address-index?address="+token);
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        // Return the wallet status
         public static IEnumerator WalletStatus()
         {
             UnityWebRequest www = UnityWebRequest.Get("http://localhost:8000/wallet/status");
@@ -80,11 +112,46 @@ namespace UnityHathor
             }
         }
 
-
-        //Retorna 1 endere�o da conta.
-        public static IEnumerator GetAddress()
+        // Return all generated addresses of the wallet.
+        public static IEnumerator WalletAddresses()
         {
-            UnityWebRequest www = UnityWebRequest.Get("http://localhost:8000/wallet/address");
+            UnityWebRequest www = UnityWebRequest.Get("http://localhost:8000/wallet/addresses");
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        //Return the current address
+        public static IEnumerator GetAddress(string mark_as_used=null, int index=null)
+        {
+            UnityWebRequest www = null;
+
+            if(mark_as_used == null && index == null){
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address");
+            } else if(mark_as_used != null && index == null ){
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address?mark_as_used="+mark_as_used);
+            } else if(mark_as_used == null && index != null ){
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address?index="+index.ToString());
+            } else if (mark_as_used != null && index != null) {
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address?mark_as_used="+mark_as_used+"&index="+index.ToString());
+            }
+
 
             www.SetRequestHeader("X-Wallet-Id", login);
 
@@ -107,7 +174,7 @@ namespace UnityHathor
         }
 
 
-        // Inicia a wallet, caso ela nao exista, ela � criada. Caso existente a API retorna que j� esta criada
+        // Create and start a wallet and add to store.
         public static IEnumerator StartWallet()
         {
             WWWForm form = new WWWForm();
@@ -132,17 +199,23 @@ namespace UnityHathor
             }
         }
 
-        // Envia para somente 1 wallet destino.
-        public static IEnumerator SendTxToPlayer(HathorTransactStruct _a)
-        {           
+        // Send a transaction to exactly one output.
+        // Old SendTxToPlayer
+        public static IEnumerator SimpleSendTxTo(HathorTransactStruct _a)
+        {
 
             WWWForm form = new WWWForm();
-            form.AddField("address", _a.WalletPlayerParam);
+            form.AddField("address", _a.Address);
             form.AddField("value", _a.HathorAmount);
+
+            if (_a.Token != null)
+                form.AddField("address", _a.Token);
+            if (_a.ChangeAddress != null)
+                form.AddField("change_address", _a.ChangeAddress);
 
             using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/simple-send-tx", form))
             {
-                www.SetRequestHeader("X-Wallet-Id", login);                
+                www.SetRequestHeader("X-Wallet-Id", login);
                 yield return www.SendWebRequest();
 
                 if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
@@ -151,17 +224,323 @@ namespace UnityHathor
                     //logInputTextValue.text = www.error;
                 }
                 else
-                {                    
+                {
                     //Debug.Log(www.downloadHandler.text);
                     if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
                     {
-                        Debug.Log("Transa��o aguardando na fila.");
+                        Debug.Log("Transaction waiting in queue.");
                         HathorQueue.transactsQueue.Enqueue(_a);
                     }
                     //logInputTextValue.text = www.downloadHandler.text;
                 }
             }
             yield return new WaitForSeconds(.3f);
+        }
+
+        // Send a transaction with many outputs.
+        public static IEnumerator SendTxTo(HathorTransactStruct _a)
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("outputs", JsonUtility.ToJson(_a.OutputsArray));
+
+            if (_a.InputsArray != null)
+                form.AddField("inputs", JsonUtility.ToJson(_a.InputsArray));
+            if (_a.ChangeAddress != null)
+                form.AddField("change_address", _a.ChangeAddress);
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/send-tx", form))
+            {
+                www.SetRequestHeader("X-Wallet-Id", login);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log(www.error);
+                    //logInputTextValue.text = www.error;
+                }
+                else
+                {
+                    //Debug.Log(www.downloadHandler.text);
+                    if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
+                    {
+                        Debug.Log("Transaction waiting in queue.");
+                        HathorQueue.transactsQueue.Enqueue(_a);
+                    }
+                    //logInputTextValue.text = www.downloadHandler.text;
+                }
+            }
+            yield return new WaitForSeconds(.3f);
+        }
+
+        // Create NFT in game
+        public static IEnumerator CreateNFT(HathorTransactStruct _a)
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("name", _a.Name);
+            form.AddField("symbol", _a.Symbol);
+            form.AddField("amount", _a.Amount);
+            form.AddField("data", _a.Data);
+
+            if (_a.Address != null)
+                form.AddField("address", _a.Address);
+            if (_a.ChangeAddress != null)
+                form.AddField("change_address", _a.ChangeAddress);
+            if (_a.CreateMint != null)
+                form.AddField("create_mint", _a.CreateMint);
+            if (_a.CreateMelt != null)
+                form.AddField("create_melt", _a.CreateMelt);
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/create-nft", form))
+            {
+                www.SetRequestHeader("X-Wallet-Id", login);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log(www.error);
+                    //logInputTextValue.text = www.error;
+                }
+                else
+                {
+                    //Debug.Log(www.downloadHandler.text);
+                    if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
+                    {
+                        Debug.Log("Transacao aguardando na fila.");
+                        HathorQueue.transactsQueue.Enqueue(_a);
+                    }
+                    //logInputTextValue.text = www.downloadHandler.text;
+                }
+            }
+            yield return new WaitForSeconds(.3f);
+        }
+
+        //Create a token.
+        public static IEnumerator CreateToken(HathorTransactStruct _a)
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("name", _a.Name);
+            form.AddField("symbol", _a.Symbol);
+            form.AddField("amount", _a.Amount);
+
+            if (_a.Address != null)
+                form.AddField("address", _a.Address);
+            if (_a.ChangeAddress != null)
+                form.AddField("change_address", _a.ChangeAddress);
+
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/create-token", form))
+            {
+                www.SetRequestHeader("X-Wallet-Id", login);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log(www.error);
+                    //logInputTextValue.text = www.error;
+                }
+                else
+                {
+                    //Debug.Log(www.downloadHandler.text);
+                    if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
+                    {
+                        Debug.Log("Transaction waiting in queue.");
+                        HathorQueue.transactsQueue.Enqueue(_a);
+                    }
+                    //logInputTextValue.text = www.downloadHandler.text;
+                }
+            }
+            yield return new WaitForSeconds(.3f);
+        }
+
+        //Mint a token.
+        public static IEnumerator MintToken(HathorTransactStruct _a)
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("token", _a.Token);
+            form.AddField("address", _a.Address);
+            form.AddField("amount", _a.Amount);
+
+            if (_a.ChangeAddress != null)
+                form.AddField("change_address", _a.ChangeAddress);
+
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/mint-tokens", form))
+            {
+                www.SetRequestHeader("X-Wallet-Id", login);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log(www.error);
+                    //logInputTextValue.text = www.error;
+                }
+                else
+                {
+                    //Debug.Log(www.downloadHandler.text);
+                    if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
+                    {
+                        Debug.Log("Transaction waiting in queue.");
+                        HathorQueue.transactsQueue.Enqueue(_a);
+                    }
+                    //logInputTextValue.text = www.downloadHandler.text;
+                }
+            }
+            yield return new WaitForSeconds(.3f);
+        }
+
+        //Melt a token.
+        public static IEnumerator MeltToken(HathorTransactStruct _a)
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("token", _a.Token);
+            form.AddField("amount", _a.Amount);
+
+            using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8000/wallet/melt-tokens", form))
+            {
+                www.SetRequestHeader("X-Wallet-Id", login);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.Log(www.error);
+                    //logInputTextValue.text = www.error;
+                }
+                else
+                {
+                    //Debug.Log(www.downloadHandler.text);
+                    if (www.downloadHandler.text.Contains("You already have a transaction being sent. Please wait until it's done to send another"))
+                    {
+                        Debug.Log("Transaction waiting in queue.");
+                        HathorQueue.transactsQueue.Enqueue(_a);
+                    }
+                    //logInputTextValue.text = www.downloadHandler.text;
+                }
+            }
+            yield return new WaitForSeconds(.3f);
+        }
+
+        // Return the data of a transaction, if it exists in the wallet
+        public static IEnumerator Transaction(string id)
+        {
+            UnityWebRequest www = null;
+
+            www = UnityWebRequest.Get("http://localhost:8000/wallet/transaction?id="+id);
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        // Return the transaction history
+        public static IEnumerator TxHistory(int limit=null)
+        {
+            UnityWebRequest www = null;
+
+            if (limit != null)
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/tx-history?limit="+limit.ToString());
+            else
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/tx-history");
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        // Stop a running wallet and remove from store.
+        public static IEnumerator WalletStop()
+        {
+            UnityWebRequest www = null;
+
+            www = UnityWebRequest.Get("http://localhost:8000/wallet/stop");
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
+        }
+
+        // Get information of a given address.
+        public static IEnumerator AddressInfo(string address, string token = null)
+        {
+            UnityWebRequest www = null;
+
+            if (token != null)
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address-info?address="+address+"&token="+token);
+            else
+                www = UnityWebRequest.Get("http://localhost:8000/wallet/address-info?address="+address);
+
+
+            www.SetRequestHeader("X-Wallet-Id", login);
+
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+                //logInputTextValue.text = www.error;
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                //logInputTextValue.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+            }
         }
 
     }
--- Scripts/HathorQueue.cs
+++ Scripts/HathorQueue.cs
@@ -17,9 +17,9 @@ namespace UnityHathor
             {
                 if (transactsQueue.Count > 0)
                 {
-                    yield return new WaitForSeconds(.3f);                    
-                    HathorTransactStruct _a = transactsQueue.Dequeue();                    
-                    StartCoroutine(HathorConnect.SendTxToPlayer(_a));                                                
+                    yield return new WaitForSeconds(.3f);
+                    HathorTransactStruct _a = transactsQueue.Dequeue();
+                    StartCoroutine(HathorConnect.SendTxToPlayer(_a));
                 }
                 yield return null;
             }
@@ -35,14 +35,105 @@ namespace UnityHathor
 
     public struct HathorTransactStruct
     {
-        public HathorTransactStruct(string walletPlayerParam, int hathorAmount)
+        public HathorTransactStruct(string address, int hathorAmount)
         {
-            WalletPlayerParam = walletPlayerParam;
+            Address = address;
             HathorAmount = hathorAmount;
         }
-        public string WalletPlayerParam { get; }
+
+        // Simple Send Tx - Send a transaction to exactly one output.
+        public HathorTransactStruct(string address, int hathorAmount, string token=null, string change_address=null)
+        {
+            Address = address;
+            HathorAmount = hathorAmount;
+            Token = token;
+            ChangeAddress = change_address;
+        }
+
+        // Send-Tx - Send a transaction with many outputs.
+        public HathorTransactStruct(Outputs[] outputs, Inputs[] inputs=null, string change_address=null)
+        {
+            OutputsArray = outputs;
+            InputsArray = inputs;
+            ChangeAddress = change_address;
+        }
+
+        // Create a token.
+        public HathorTransactStruct(string name, string symbol, int amount, string address=null, string change_address=null)
+        {
+            Name = name;
+            Symbol = symbol;
+            Amount = amount;
+            Address = address;
+            ChangeAddress = change_address;
+        }
+
+        // Mint tokens.
+        public HathorTransactStruct(string token, int amount)
+        {
+            Token = token;
+            Amount = amount;
+        }
+
+        // Melt tokens.
+        public HathorTransactStruct(string token, string address, int amount, string change_address=null)
+        {
+            Token = token;
+            Add = symbol;
+            Amount = amount;
+            ChangeAddress = change_address;
+        }
+
+        // Create NFT
+        public HathorTransactStruct(string name, string symbol, int amount, string data, string address=null, string change_address=null, bool create_mint=null, bool create_melt=null)
+        {
+            Name = name;
+            Symbol = symbol;
+            Amount = amount;
+            Data = data;
+            Address = address;
+            ChangeAddress = change_address;
+            CreateMint = create_mint;
+            CreateMelt = CreateMelt;
+        }
+
+        public string Name { get; }
+        public string Symbol { get; }
+        public string Amount { get; }
+        public string Data { get; }
+        public string Token { get; }
+        public string ChangeAddress { get; }
+        public bool CreateMint { get; }
+        public bool CreateMelt { get; }
+        public string Address { get; }
         public int HathorAmount { get; }
+        public Outputs[] OutputsArray { get; }
+        public Inputs[] InputsArray { get; }
+
+
     };
+
+    [Serializable]
+    public class Outputs
+    {
+        public string address;
+        public int value;
+        public string token;
+    }
+
+    [Serializable]
+    public class Inputs
+    {
+        public string hash;
+        public int index;
+        public string type;
+        public int max_utxos;
+        public string token;
+        public string filter_address;
+        public int amount_smaller_than;
+        public int amount_bigger_than;
+    }
 }
+
 
 
